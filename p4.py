aee2df30ede20e95386c1f3292b4e21f0fdca1bc
M <sciguymjm@gmail.com>
2018-06-07 15:20:59 -0400

    basic gaussian mixture model

--- p4.py
+++ p4.py
@@ -1,5 +1,3 @@
-import sys
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.special import erfinv
@@ -41,6 +39,15 @@ def normal(mu, r):
     return x
 
 
+def inv_erf(z):
+    def c_k(k):
+        if k < 2:
+            return 1
+        return np.sum([(c_k(m) * c_k(k - 1 - m) / ((m + 1) * (2 * m + 1))) for m in range(0, k - 1)])
+
+    return np.sum([c_k(k) / (2 * k + 1) * (np.sqrt(np.math.pi / 2 * z)) ** (2 * k + 1) for k in range(0, 10)])
+
+
 def test_custom_normal():
     data = [normal(0, 1) for x in range(10000)]
     print('std', np.std(data), 'mean', np.mean(data))
@@ -62,17 +69,32 @@ def plot_pt(val, cls):
     plt.plot(val[0], val[1], 'o', color='green' if cls == 1 else 'red')
 
 
+mus = [
+    [[0., 0.]],
+    [[2., 5.]]
+]
+covs = [
+    [[5., 0.], [0., 10.]],
+    [[1., 0.], [0., 10.]]
+]
+
+
+def generate_sample_gmm():
+    n = 2
+    prob = [0.5, 0.5]
+    idx = np.arange(n)
+    choice = np.random.choice(idx, p=prob)
+    return np.random.multivariate_normal(mus[choice][0], covs[choice]), choice
+
+
+
 if __name__ == '__main__':
+    print (generate_sample_gmm())
     test_custom_normal()
     # multivar_normal([0,0], [0,0], 5000)
-    mu1 = [[0., 0.]]
-    mu2 = [[5., 5.]]
-    cov1 = [[10., 0.], [0., 10.]]
-    cov2 = [[1., 0.], [0., 10.]]
-    mu1 = np.array(mu1).T
-    mu2 = np.array(mu2).T
-    cov1 = np.array(cov1)
-    cov2 = np.array(cov2)
+
+    mus_t = [np.array(m).T for m in mus]
+    covs_t = [np.array(c) for c in covs]
     sampled_values = ((np.random.rand(1000, 2, 1) - 0.5) * 40)
     samples = []
 
@@ -89,7 +111,7 @@ if __name__ == '__main__':
         i, j = sampled_value[0]
         sampled_value = np.array(sampled_value[1])[np.newaxis].T
         vals = []
-        for mu, cov in zip([mu1, mu2], [cov1, cov2]):
+        for mu, cov in zip(mus_t, covs_t):
             st1 = 1 / (2 * np.math.pi * np.sqrt(np.linalg.det(cov)))
             # print('st1', st1)
             # print('(sampled_value - mu).transpose()', (sampled_value - mu).T)
@@ -104,10 +126,11 @@ if __name__ == '__main__':
         results[i, j] = vals[1] / vals[0]
         samples.append([sampled_value, vals])
     threshold = 1
-    mpl(mu1.T[0], cov1, color='yellow')
-    mpl(mu2.T[0], cov2, color='orange')
+    mpl(mus_t[0].T[0], covs_t[0], color='yellow')
+    mpl(mus_t[1].T[0], covs_t[1], color='orange')
     # [plot_pt(sample, 1 if (val[1] / val[0]) > threshold else 0) for sample, val in samples]
-    c = plt.contour(X, Y, results, [10 ** x for x in np.arange(-2, 3, 0.5)], zorder=1000)
+    c = plt.contour(X, Y, results, [1], zorder=1000)
+    # c = plt.contour(X, Y, results, [10 ** x for x in np.arange(-2, 3, 0.5)], zorder=1000)
     plt.clabel(c, inline=1, fontsize=5)
-    print(results)
+    # print(results)
     mpl_show()
